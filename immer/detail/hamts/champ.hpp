ce39e14b02e6c51edbb2c796bfdd9810cbf0eb6d
Fabian Schiebel <fabian.schiebel@iem.fraunhofer.de>
2024-04-25 10:04:19 +0200

    Inline try_update

--- immer/detail/hamts/champ.hpp
+++ immer/detail/hamts/champ.hpp
@@ -10,6 +10,7 @@
 
 #include "immer/detail/util.hpp"
 #include <immer/config.hpp>
+#include <immer/detail/hamts/bits.hpp>
 #include <immer/detail/hamts/node.hpp>
 
 #include <algorithm>
@@ -925,15 +926,15 @@ struct champ
               typename ValueEquals>
     struct TryUpdater
     {
-        static update_result
+        template <shift_t shift>
+        static IMMER_FORCEINLINE update_result
         do_try_update(node_t* node,
                       byval_if_possible<K> k,
                       Fn fn,
                       byval_if_possible<ValueEquals> valueEquals,
-                      hash_t hash,
-                      shift_t shift)
+                      hash_t hash)
         {
-            if (shift == max_shift<B>) {
+            if constexpr (shift == max_shift<B>) {
                 auto fst = node->collisions();
                 auto lst = fst + node->collision_count();
                 for (; fst != lst; ++fst)
@@ -961,12 +962,8 @@ struct champ
                 auto bit = bitmap_t{1u} << idx;
                 if (node->nodemap() & bit) {
                     auto offset = node->children_count(bit);
-                    auto result = do_try_update(node->children()[offset],
-                                                k,
-                                                fn,
-                                                valueEquals,
-                                                hash,
-                                                shift + B);
+                    auto result = do_try_update<shift + B>(
+                        node->children()[offset], k, fn, valueEquals, hash);
                     if (!result.node)
                         return result;
 
@@ -1023,16 +1020,16 @@ struct champ
             }
         }
 
-        static update_mut_result
+        template <shift_t shift>
+        static IMMER_FORCEINLINE update_mut_result
         do_try_update_mut(edit_t e,
                           node_t* node,
                           byval_if_possible<K> k,
                           Fn fn,
                           byval_if_possible<ValueEquals> valueEquals,
-                          hash_t hash,
-                          shift_t shift)
+                          hash_t hash)
         {
-            if (shift == max_shift<B>) {
+            if constexpr (shift == max_shift<B>) {
                 auto fst = node->collisions();
                 auto lst = fst + node->collision_count();
                 for (; fst != lst; ++fst)
@@ -1072,8 +1069,8 @@ struct champ
                     auto offset = node->children_count(bit);
                     auto child  = node->children()[offset];
                     if (node->can_mutate(e)) {
-                        auto result = do_try_update_mut(
-                            e, child, k, fn, valueEquals, hash, shift + B);
+                        auto result = do_try_update_mut<shift + B>(
+                            e, child, k, fn, valueEquals, hash);
                         if (!result.node)
                             return result;
 
@@ -1082,8 +1079,8 @@ struct champ
                             node_t::delete_deep_shift(child, shift + B);
                         return {node, result.added, true};
                     } else {
-                        auto result = do_try_update(
-                            child, k, fn, valueEquals, hash, shift + B);
+                        auto result = do_try_update<shift + B>(
+                            child, k, fn, valueEquals, hash);
                         if (!result.node)
                             return {nullptr, false, false};
 
@@ -1177,18 +1174,17 @@ struct champ
     champ try_update(const K& k, Fn&& fn, ValueEquals valueEquals = {}) const
     {
         auto hash = Hash{}(k);
-        auto res =
-            TryUpdater<Project,
-                       Default,
-                       Combine,
-                       std::decay_t<K>,
-                       std::decay_t<Fn>,
-                       ValueEquals>::do_try_update(root,
-                                                   k,
-                                                   detail::ref_or_fwd<Fn>(fn),
-                                                   std::move(valueEquals),
-                                                   hash,
-                                                   0);
+        auto res  = TryUpdater<
+            Project,
+            Default,
+            Combine,
+            std::decay_t<K>,
+            std::decay_t<Fn>,
+            ValueEquals>::template do_try_update<0>(root,
+                                                    k,
+                                                    detail::ref_or_fwd<Fn>(fn),
+                                                    std::move(valueEquals),
+                                                    hash);
         if (!res.node)
             return {root->inc(), size};
 
@@ -1410,20 +1406,19 @@ struct champ
     void try_update_mut(edit_t e, K&& k, Fn&& fn, ValueEquals valueEquals)
     {
         auto hash = Hash{}(k);
-        auto res =
-            TryUpdater<Project,
-                       Default,
-                       Combine,
-                       std::decay_t<K>,
-                       std::decay_t<Fn>,
-                       ValueEquals>::do_try_update_mut(e,
-                                                       root,
-                                                       std::forward<K>(k),
-                                                       detail::ref_or_fwd<Fn>(
-                                                           fn),
-                                                       valueEquals,
-                                                       hash,
-                                                       0);
+        auto res  = TryUpdater<
+            Project,
+            Default,
+            Combine,
+            std::decay_t<K>,
+            std::decay_t<Fn>,
+            ValueEquals>::template do_try_update_mut<0>(e,
+                                                        root,
+                                                        std::forward<K>(k),
+                                                        detail::ref_or_fwd<Fn>(
+                                                            fn),
+                                                        valueEquals,
+                                                        hash);
         if (!res.node)
             return;
 
